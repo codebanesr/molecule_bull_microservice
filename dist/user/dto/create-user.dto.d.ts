2e162d68e2259cbecb696254369a77a9e8c654ff
shanurrahman <shanur.cse.nitap@gmail.com>
2021-02-12 23:19:43 +0530

    fix: [BN-1.5] removing mongo from docker file

---
+++ dist/user/dto/create-user.dto.d.ts
@@ -0,0 +1,9 @@
+export declare class CreateUserDto {
+    readonly fullName: string;
+    readonly email: string;
+    password: string;
+    readonly roleType: string;
+    readonly reportsTo: string;
+    readonly roles: string[];
+    phoneNumber: string;
+}
