bf6b5f4a0288a51c7dc898d1c38572e7fef44703
shanurrahman <shanur.cse.nitap@gmail.com>
2021-02-25 01:33:19 +0530

    removed static files, will serve from netlify

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const dotenv_1 = require("dotenv");
 dotenv_1.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, {
-        logger: ['debug']
+        logger: console
     });
     app.listen(process.env.BULL_APP_PORT);
 }
